4e949574bdc24a27f225dde5ed4bd220d015b5cd
caop72 <ontivero72@gmail.com>
2025-09-26 17:45:18 -0400

    Create conexión_pool.js
    para inicializar

---
+++ config/conexión_pool.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/formulario_r';
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10, // Tamaño máximo del pool de conexiones
+};
+
+mongoose.connect(uri, options)
+  .then(() => console.log('Conectado a MongoDB con pool'))
+  .catch(err => console.error('Error al conectar a MongoDB:', err));
+
+module.exports = mongoose;
